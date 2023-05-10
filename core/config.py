e095f6895600324d24f67563acb83dd64fb53373
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-10 15:18:18 +0300

    Created the Settings class

--- core/config.py
+++ core/config.py
@@ -1,2 +1,11 @@
 """The file that will contain the configuration logic for the application"""
 from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """The settings class
+
+    Args:
+        BaseSettings (Pydantic): Contains the base logic for defining the settings logic
+    """
+    pass
