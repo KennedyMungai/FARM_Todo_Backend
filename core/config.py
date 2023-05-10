f955beebba9bf525519b2a27630f3ca5f07aee34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-10 15:34:42 +0300

    Added a bunch more imports to the python script

--- core/config.py
+++ core/config.py
@@ -1,8 +1,9 @@
 """The file that will contain the configuration logic for the application"""
 import os
+from typing import List
 
 from dotenv import find_dotenv, load_dotenv
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyHttpUrl
 
 load_dotenv(find_dotenv())
 
@@ -14,3 +15,8 @@ class Settings(BaseSettings):
         BaseSettings (Pydantic): Contains the base logic for defining the settings logic
     """
     JWT_SECRET_KEY: str = os.environ.get("JWT_SECRET_KEY")
+    JWT_REFRESH_SECRET_KEY: str = os.environ.get("JWT_REFRESH_SECRET_KEY")
+    ALGORITHM = 'HS256'
+    ACCESS_TOKEN_EXPIRATION_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRATION_MINUTES: int = 60 * 24 * 7         # 7 days
+    PROJECT_NAME: str = "Todo App"
