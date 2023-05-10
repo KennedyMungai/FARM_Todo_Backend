25db55fb1a5329ace9f7e978211c511a762e377b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-10 15:57:22 +0300

    Wrote the individual fields of the User class

--- models/user_model.py
+++ models/user_model.py
@@ -1,2 +1,19 @@
 """The User Model """
 from beanie import Document, Indexed
+from uuid import UUID, uuid4
+from pydantic import Field, EmailStr
+
+
+class User(Document):
+    """The User Model;
+
+    Args:
+        Document (Document): A data type resembling the MongoDB document
+    """
+    user_id: UUID = Field(default_factory=uuid4)
+    username: str = Indexed(str, unique=True)
+    email: EmailStr = Indexed(EmailStr, unique=True)
+    hashed_password: str
+    first_name: str
+    last_name: str
+    disabled: bool
