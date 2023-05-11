5925d57c75645d179c1acfa526ceae468baaa7e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 09:42:52 +0300

    Created the UserAuth schema

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,2 +1,14 @@
 """The file with the user schemas"""
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserAuth(BaseModel):
+    """The schema used by the user when authenticating
+
+    Args:
+        BaseModel (Pydantic): The base library for the UserAuth schema
+    """
+    email: EmailStr = Field(..., description="User Email")
+    username: str = Field(..., min_length=5, max_length=50,
+                          description="The username of the User")
+    password: str = Field(..., min_length=5, max_length=24)
