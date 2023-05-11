9e3b42f65e97c962f7ce31930d4f4e118bf7d690
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 09:58:06 +0300

    Created a function to hash the password

--- core/security.py
+++ core/security.py
@@ -3,3 +3,12 @@ from passlib.context import CryptContext
 
 
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def hash_password(password: str) -> str:
+    """
+    Hashes a password using the passlib library
+    :param password: the password to be hashed
+    :return: the hashed password
+    """
+    return password_context.hash(password)
