02cc985ec8f5e752bf1a4355936836d6f005fbd1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 10:31:57 +0300

    Returne dth eresult of the create_user function

--- routers/user_router.py
+++ routers/user_router.py
@@ -40,7 +40,7 @@ async def create_user_router(data: UserAuth):
         User: Returns the newly created user
     """
     try:
-        await UserService.create_user(data)
+        return await UserService.create_user(data)
     except pymongo.errors.DuplicateKeyError as error:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
