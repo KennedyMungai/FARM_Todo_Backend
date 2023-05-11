21bf72589a740b9f38762c03db87f821583cca54
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 09:56:40 +0300

    Imported CryptContext from passlib.context and created an instacnce if CryptContext called password_context

--- core/security.py
+++ core/security.py
@@ -1,1 +1,5 @@
 """The file that contains the security for the backend application"""
+from passlib.context import CryptContext
+
+
+password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
