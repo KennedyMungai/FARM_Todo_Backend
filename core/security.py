0fda8a8c0f1338dd9bc3230dba6c89f6da5d505c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 09:58:44 +0300

    Wrote a function to verify the password

--- core/security.py
+++ core/security.py
@@ -12,3 +12,7 @@ def hash_password(password: str) -> str:
     :return: the hashed password
     """
     return password_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_context.verify(plain_password, hashed_password)
