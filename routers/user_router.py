3335a839829ff0f1d3ad2cb229e72542e9990a30
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 09:03:27 +0300

    Defined the default route for the user

--- routers/user_router.py
+++ routers/user_router.py
@@ -2,3 +2,18 @@
 from fastapi import APIRouter
 
 user_router = APIRouter(prefix="/user", tags=["User"])
+
+
+@user_router.get(
+    "/",
+    name="Default User Router",
+    description="The root route for Users",
+    tags=["Users"]
+)
+async def user_test():
+    """The default user router used for testing user routes
+
+    Returns:
+        dict[str, str]: Returns a message to show good execution
+    """
+    return {"message": "The user route is working"}
