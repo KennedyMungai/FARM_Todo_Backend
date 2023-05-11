d8076ce4d79c9d1f196bb2e3c7b48337dc2252f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 09:50:31 +0300

    Wrote the bare minumum of the create user static method for the UserService class

--- services/user_service.py
+++ services/user_service.py
@@ -1,1 +1,13 @@
 """The script to hold the user services"""
+from schemas.user_schema import UserAuth
+from models.user_model import User
+
+
+class UserService:
+    @staticmethod
+    async def create_user(user: UserAuth):
+        user_in = User(
+            username=user.username,
+            email=user.email,
+            hashed_password=user.password
+        )
