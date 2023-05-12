4b5a96ac5a1f0fae922d1d3b92d5ecd897baa30b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 12:28:26 +0300

    Made the login endpoint output any

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,13 +1,14 @@
 """The JWT authentication file"""
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from typing import Any
 
 
 auth_router = APIRouter(prefix="/login", tags=["auth"])
 
 
 @auth_router.post("/")
-async def login(login: OAuth2PasswordRequestForm = Depends()):
+async def login(login: OAuth2PasswordRequestForm = Depends()) -> Any:
     """
     Login endpoint
     """
