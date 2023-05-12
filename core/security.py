c496803a190c7f3a15fbfdf866e409f6199be34c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 13:01:50 +0300

    Wrote all the logic of the create_access_token function

--- core/security.py
+++ core/security.py
@@ -28,6 +28,16 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 def create_access_token(_subject: Union[str, Any], _expires_delta: int = None) -> str:
+    """Function creates the access token
+
+    Args:
+        _subject (Union[str, Any]): The subject of the token.
+        _expires_delta (int, optional): The expiration time of the token.
+        Defaults to None.
+
+    Returns:
+        str: The access token
+    """
     if _expires_delta is not None:
         _expires_delta = datetime.utcnow() + _expires_delta
     else:
@@ -35,3 +45,7 @@ def create_access_token(_subject: Union[str, Any], _expires_delta: int = None) -
         ) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRATION_MINUTES)
 
     to_encode = {"exp": _expires_delta, "sub": str(_subject)}
+    encoded_jwt = jwt.encode(
+        to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
+
+    return encoded_jwt
