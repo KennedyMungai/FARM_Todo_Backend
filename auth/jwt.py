9fad0abcd2d216fc6d46f679fea2546f2c790675
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 12:48:07 +0300

    Raised a 401 if the user does not exist

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,7 +1,7 @@
 """The JWT authentication file"""
 from typing import Any
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from services.user_service import UserService
@@ -14,4 +14,10 @@ async def login(_form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     """
     Login endpoint
     """
-    _user = await UserService.authenticate(email=_form_data.email, password=_form_data.password)
+    _user = await UserService.authenticate(_email=_form_data.email, _password=_form_data.password)
+
+    if not _user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password"
+        )
