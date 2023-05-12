2b219fe0f2de854ad4eb626b88d20cfa5747154e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 12:29:37 +0300

    Added an import for the UserService class

--- auth/jwt.py
+++ auth/jwt.py
@@ -4,6 +4,9 @@ from typing import Any
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
+from services.user_service import UserService
+
+
 auth_router = APIRouter(prefix="/login", tags=["auth"])
 
 
