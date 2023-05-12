65d31ef265b389955b1b34a5e6663c2432ad66d7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 12:58:51 +0300

    Imported jwt from python jose

--- core/security.py
+++ core/security.py
@@ -1,5 +1,9 @@
 """The file that contains the security for the backend application"""
+from typing import Any, Union
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from core.config import settings
+from jose import jwt
 
 
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -21,3 +25,13 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         bool: _description_
     """
     return password_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(_subject: Union[str, Any], _expires_delta: int = None) -> str:
+    if _expires_delta is not None:
+        _expires_delta = datetime.utcnow() + _expires_delta
+    else:
+        _expires_delta = datetime.utcnow(
+        ) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRATION_MINUTES)
+
+    to_encode = {"exp": _expires_delta, "sub": str(_subject)}
